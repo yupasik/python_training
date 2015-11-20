402b98a381842cba75b5f91ff4e6961c655c71c9
Yuri <yupasik@gmail.com>
2015-11-20 13:18:14 +0300

    Выделены классы для работы с группами и контактами

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -12,7 +12,7 @@ def app(request):
 
 def test_test_add_contact(app):
     app.session.login(username="admin", password="secret")
-    app.create_contact(Contact(first_name="Yuri", middle_name="none", last_name="Perin", nickname="yupasik",
+    app.contact.create_contact(Contact(first_name="Yuri", middle_name="none", last_name="Perin", nickname="yupasik",
                    title="None", company="GS", address="Spb, Turistskay 28k1, 138", home_phone="none",
                    mobile_phone="+79219709149", work_phone="none", fax="none", email_1="yupasik@gmail.com",
                    email_2="none", email_3="none", homepage="none", b_day="18", b_month="8", a_day="14", a_month="3",
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -12,11 +12,11 @@ def app(request):
 
 def test_add_group(app):
     app.session.login(username="admin", password="secret")
-    app.create_group(Group(name="afgag", header="asdfasdf", footer="asdfasd"))
+    app.group.create(Group(name="afgag", header="asdfasdf", footer="asdfasd"))
     app.session.logout()
 
 def test_add_empty_group(app):
     app.session.login(username="admin", password="secret")
-    app.create_group(Group(name="", header="", footer=""))
+    app.group.create(Group(name="", header="", footer=""))
     app.session.logout()
 
